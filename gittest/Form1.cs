2b2bfebca8c405cdf7788a237309e6b8b7676f8b
Sergey Tilchin <sergeyt@retalix.com>
2016-04-17 09:56:15 +0300

    initial

--- gittest/Form1.cs
+++ gittest/Form1.cs
@@ -12,9 +12,14 @@ namespace gittest
 {
     public partial class Form1 : Form
     {
-        public Form1()
+        public Form1() 
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
