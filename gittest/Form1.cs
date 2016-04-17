c6c30cabfa7f1b9c581b89a7f973ee5c78d5c1ba
Sergey Tilchin <sergeyt@retalix.com>
2016-04-17 10:08:00 +0300

    commit3

--- gittest/Form1.cs
+++ gittest/Form1.cs
@@ -14,10 +14,10 @@ namespace gittest
     {
         public Form1() 
         {
-            InitializeComponent();  
+            InitializeComponent();   
         }
 
-        private void Form1_Load(object sender, EventArgs e)
+        private void Form1_Load(object sender, EventArgs e) 
         {
 
         }
